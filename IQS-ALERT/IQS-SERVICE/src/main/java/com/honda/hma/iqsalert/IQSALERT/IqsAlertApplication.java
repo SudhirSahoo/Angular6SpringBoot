4d1187e41a7a02b8fb3b178a3b5799ef0e2295bc
SudhirSahoo <sudhirid@gmail.com>
2018-07-12 16:25:14 -0500

    Add files via upload

---
+++ IQS-ALERT/IQS-SERVICE/src/main/java/com/honda/hma/iqsalert/IQSALERT/IqsAlertApplication.java
@@ -0,0 +1,12 @@
+package com.honda.hma.iqsalert.IQSALERT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqsAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqsAlertApplication.class, args);
+	}
+}
